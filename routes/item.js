65ea94aea31df0be1c7ac17a5059f0c51985a30c
element190 <farindebabajide@gmail.com>
2023-11-05 21:53:41 +0100

    add id

--- routes/item.js
+++ routes/item.js
@@ -100,7 +100,7 @@ route.patch("/edit-items-by-id/:id",adminsOnly, async (req,res)=>{
 
 route.delete("delete/:id",adminsOnly,async (req,res)=>{
     try{
-        const deletedTask = await itemCollection.findByIdAndDelete({id:req.params.id});
+        const deletedTask = await itemCollection.findByIdAndDelete({_id:req.params.id});
         if(!deletedTask) return res.status(400).send("Not Found");
         res.status(200).send("Task has been deleted successfully!");
     } catch (error) {
